d2073c949817e3637c05a63199f1b770a15e5ae0
Nakul kankarwal <79783298+nakulk02@users.noreply.github.com>
2024-10-27 21:50:23 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,10 +4,6 @@ const verifyJWT = require('../middleware/verifyJWT.js');
 const router = Router();
 
 router.post('/login', authController.login_post);
-// router.get('/',(req,res)=>{
-//     res.send({title:"hello"});
-// });
-
 router.get('/searching/:search',verifyJWT, authController.searching_get);
 router.get('/logout',authController.logout_get);
 router.post('/otp',verifyJWT,authController.otp_post);
